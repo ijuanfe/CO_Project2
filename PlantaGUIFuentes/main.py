0d7b6c5bb45f2277faf9cf8b015042f2f2848b21
vvbv <jeison.cardona@mgwebstudio.com>
2020-10-29 17:51:21 -0500

    xd * Leonardo DiCaprio meme *

--- PlantaGUIFuentes/main.py
+++ PlantaGUIFuentes/main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog, QApplication, QComboBox, QPushButton, QMainWindow
+from PyQt5.QtWidgets import QSpinBox, QApplication, QLineEdit, QPushButton, QMainWindow
 
 
 class Ui(QMainWindow):
@@ -9,6 +9,16 @@ class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('ui/main.ui', self)
+
+        self.days = self.findChild(QSpinBox, 'spinBox')
+
+        self.NCaP = self.findChild(QLineEdit, "lineEdit")
+        self.HCaP = self.findChild(QLineEdit, "lineEdit_2")
+        self.TCaP = self.findChild(QLineEdit, "lineEdit_3")
+        self.NCoP = self.findChild(QLineEdit, "lineEdit_6")
+        self.HCoP = self.findChild(QLineEdit, "lineEdit_5")
+        self.TCoP = self.findChild(QLineEdit, "lineEdit_4")
+
         self.initializeUI()
 
     def initializeUI(self):
